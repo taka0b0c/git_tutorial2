8221ceb76408c49f114d660cf96ae4e020feb880
taka0b0c <taka0b0c@gmail.com>
2022-11-18 15:38:32 +0900

    11/18の宿題分

--- practice.py
+++ practice.py
@@ -1,13 +1,17 @@
 import pyautogui
 import time
 import pyperclip
+
+pyautogui.FAILSAFE = True
+
 def pyautogui_move_and_click(x_position,y_position):
-    pyautogui.moveTo(x_position,y_position)
+    pyautogui.moveTo(x_position,y_position,duration=3, tween=pyautogui.easeInOutQuad)
     pyautogui.click()
+
 pyautogui_move_and_click(759,877)
 time.sleep(5)
 pyautogui_move_and_click(597,107)
-time.sleep(10)
+time.sleep(5)
 pyautogui_move_and_click(93,628)
 time.sleep(5)
 pyautogui_move_and_click(278,222)
