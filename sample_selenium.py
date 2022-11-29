fae8dfc2ff16068cd04ed4e2f9d7739662d8e7d4
taka0b0c <taka0b0c@gmail.com>
2022-11-30 08:12:58 +0900

    再修正

--- sample_selenium.py
+++ sample_selenium.py
@@ -5,17 +5,16 @@ import time
 
 
 class Access:
-    def practice(self):
+    driver.get("https://docs.python.org/ja/3/")
+
+    def __init__(self):
         self.webdriver.Chrome('chromedriver.exe')
-        self.driver.get("https://docs.python.org/ja/3/")
+
       
 
-  def element_action(self,xpath, action, send_param):
-      self = driver.find_element(by=By.XPATH, value=xpath)
+    def element_action(self,xpath, action, send_param):
+        self.driver.find_element(by=By.XPATH, value=xpath)
       if action == "click":
          driver_element.click()
       else:
          driver_element.send_keys(send_param)
-
-access = Access()
-access.practice,element_action
