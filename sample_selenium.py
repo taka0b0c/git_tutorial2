8d5b1117f343d9a5fb9fccf2f013d5a789f66e3e
taka0b0c <taka0b0c@gmail.com>
2022-11-30 14:33:47 +0900

    再更新

--- sample_selenium.py
+++ sample_selenium.py
@@ -5,12 +5,15 @@ import time
 
 
 class Access:
-    driver.get("https://docs.python.org/ja/3/")
+    jump = driver.get
+
+Access.jump = "https://docs.python.org/ja/3/"
 
     def __init__(self):
-        self.webdriver.Chrome('chromedriver.exe')
+        self = webdriver.Chrome
 
-      
+self = 'chromedriver.exe'
+
 
     def element_action(self,xpath, action, send_param):
         self.driver.find_element(by=By.XPATH, value=xpath)
