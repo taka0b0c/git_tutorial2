df5fe044c1b6c84f60bd3b86a534a83ddda06b10
taka0b0c <taka0b0c@gmail.com>
2022-12-05 09:42:07 +0900

    python no

--- practice.py
+++ practice.py
@@ -1,24 +1,41 @@
-import pyautogui
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-import pyperclip
 
-pyautogui.FAILSAFE = True
+class Access:
 
-def pyautogui_move_and_click(x_position,y_position):
-    pyautogui.moveTo(x_position,y_position,duration=3, tween=pyautogui.easeInOutQuad)
-    pyautogui.click()
+  driver = ""
 
-pyautogui_move_and_click(759,877)
-time.sleep(5)
-pyautogui_move_and_click(597,107)
-time.sleep(5)
-pyautogui_move_and_click(93,628)
-time.sleep(5)
-pyautogui_move_and_click(278,222)
-time.sleep(5)
-pyautogui_move_and_click(550,784)
-time.sleep(5)
-pyperclip.copy("pyautoguiで送付しています")
-pyautogui.hotkey('ctrl', 'v')
-time.sleep(5)
-pyautogui_move_and_click(891,834)
+  def __init__(self):
+    self.driver = webdriver.Chrome('chromedriver.exe')
+    self.driver.get("https://terakoya.sejuku.net/home")
+
+
+  def element_action(self,xpath, action, send_param):
+    driver_element = self.driver.find_element(by=By.XPATH, value=xpath)
+    if action == "click":
+      driver_element.click()
+    else:
+      driver_element.send_keys(send_param)
+
+
+
+if __name__ == '__main__':
+  sample_selenium = Access()
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/header/div[2]/div[2]/div","click","")
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/div[1]/div[1]/div[2]/div/div[1]/input","","taka0b0c@gmail.com")
+  time.sleep(2)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/div[1]/div[1]/div[2]/div/div[2]/input","","自分のパスワード")
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/div[1]/div[1]/div[2]/button","click","")
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/nav/ul/li[9]/a/p","click","")
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/main/div/div/div[1]/div/div[2]/p[1]","click","")
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/main/div/div[3]/section[2]/div/div/textarea","","宿題で送信")
+  time.sleep(5)
+  sample_selenium.element_action("/html/body/div[1]/div[1]/div/div/main/div/div[3]/section[2]/button/img","click","")
+  time.sleep(5)
