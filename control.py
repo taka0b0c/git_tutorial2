8be9ff3ff59f69dc1a01a6a8f884bd5f97fe90fd
taka0b0c <taka0b0c@gmail.com>
2022-12-09 13:05:29 +0900

    new file

--- control.py
+++ control.py
@@ -5,26 +5,27 @@ import pyperclip
 class Access:
 
   
-  def __init__(self,x,y):
-    self.x = x
-    self.y = y
+  def __init__(self,x_position,y_position):
+    self.x_position = x
+    self.y_position = y
 
-  def click(self, cliks=1,button='left', pause=1):
+  def click(self, cliks=1, button='left', pause=1):
     pyautogui.click(self.x, self.y, clicks=cliks,button=button, pause=pause)
 
 if __name__ == "__main__":
+  control = Access()
+  
+control.click(759,877,duration=3)
 
-  pyautogui.click(759,877,duration=3)
+  #pyautogui.click(597,107,duration=3)
+  #time.sleep(10)
+  #pyautogui.click(93,628,duration=3)
 
-  pyautogui.click(597,107,duration=3)
-  time.sleep(10)
-  pyautogui.click(93,628,duration=3)
+ #pyautogui.click(278,222,duration=3)
 
-  pyautogui.click(278,222,duration=3)
+  #pyautogui.click(550,784,duration=3)
 
-  pyautogui.click(550,784,duration=3)
+  #pyperclip.copy("pyautoguiで送付しています ※class化")
+  #pyautogui.hotkey('ctrl', 'v')
 
-  pyperclip.copy("pyautoguiで送付しています ※class化")
-  pyautogui.hotkey('ctrl', 'v')
-
-  pyautogui.click(940,797,duration=3)
+  #pyautogui.click(940,797,duration=3)
